21a244c4fe280463affb54d92f5df55a2e647378
Ben-Weber <benfweber@gmail.com>
2021-08-09 21:32:02 +0300

    mid-stuff

--- data/usersdb.js
+++ data/usersdb.js
@@ -1,12 +1,17 @@
-// const query = require("./db");
+const query = require("./db");
 // const SQL = require("@nearform/sql");
 
-
-// const registerUser = async (user) => {
-//   try {
-//     const queryResult = await query(SQL )
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// exports.registerUser = registerUser;
+const registerUser = async (user) => {
+  console.log("user from user.db", user);
+  try {
+    const { email, firstName, lastName, password, phone } = user;
+    console.log("inside try", user);
+    let sql = `INSERT INTO users (email, firstName, lastName, password, phone) VALUES ('${email}', '${firstName}', '${lastName}', '${password}', ${phone})`;
+    const queryResult = await query(sql);
+    console.log("queryResult", queryResult);
+    return queryResult;
+  } catch (error) {
+    console.log(error);
+  }
+};
+exports.registerUser = registerUser;
