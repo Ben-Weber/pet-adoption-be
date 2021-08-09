56848994677f8bdd93476cd9aab5673cb041926d
Ben-Weber <benfweber@gmail.com>
2021-08-09 21:32:24 +0300

    midstuff

--- routes/user.js
+++ routes/user.js
@@ -5,14 +5,17 @@ const cors = require("cors");
 api.use(cors());
 const router = express.Router();
 const { query } = require("../data/db");
+const { registerUser } = require("../data/usersdb");
 
 // Register New User
 router.post("/addNewUser", async (req, res) => {
   try {
-    const { email, firstName, lastName, password, phone } = req.body;
-    console.log(req.body);
-    let sql = `INSERT INTO users (email, firstName, lastName, password, phone) VALUES ('${email}', '${firstName}', '${lastName}', '${password}', ${phone})`;
-    await query(sql), res.send("WORKED");
+    // const { email, firstName, lastName, password, phone } = req.body;
+    // console.log(req.body);
+    // let sql = `INSERT INTO users (email, firstName, lastName, password, phone) VALUES ('${email}', '${firstName}', '${lastName}', '${password}', ${phone})`;
+
+    registerUser(req.body.user);
+    res.send(req.body.user);
   } catch (error) {
     res.status(400).send(error.message);
     console.log(error);
