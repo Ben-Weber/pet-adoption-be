246c51d88a8554dbe7d7ec7ecf943fa494302df2
Ben-Weber <benfweber@gmail.com>
2021-08-10 13:44:39 +0300

    changed route to /signup (from /addNewUser)

--- routes/user.js
+++ routes/user.js
@@ -7,10 +7,10 @@ const router = express.Router();
 const { registerUser } = require("../data/usersdb");
 
 // Register New User
-router.post("/addNewUser", async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     registerUser(req.body);
-    res.send("Success - post request from router.user/addNewUser");
+    res.send("Success - post request from router.user/signup");
   } catch (error) {
     res.status(400).send(error.message);
     console.log(error);
